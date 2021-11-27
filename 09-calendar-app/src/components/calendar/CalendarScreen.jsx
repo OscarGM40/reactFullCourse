be5cb30f5ab5c71760364b8c9eebdcee10155886
Oscar <oscargm28@hotmail.com>
2021-11-27 18:33:12 +0100

    staging

--- 09-calendar-app/src/components/calendar/CalendarScreen.jsx
+++ 09-calendar-app/src/components/calendar/CalendarScreen.jsx
@@ -1,8 +1,14 @@
 import { Calendar as BigCalendar, momentLocalizer } from 'react-big-calendar'
 import Navbar from "../ui/Navbar"
 import moment from 'moment'
-import 'react-big-calendar/lib/css/react-big-calendar.css'
 
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { messages } from '../../helpers/calendar-messages'
+
+import 'moment/locale/es'
+import { CalendarEvent } from './CalendarEvent'
+import { useState } from 'react'
+moment.locale('es')
 
 // Setup the localizer by providing the moment Object
 // to the correct localizer.
@@ -17,12 +23,51 @@ const events = [
     title: 'Cumpleaños del jefe',
     start: moment().toDate(), //lo mismo que un new Date()
     end: moment().add(2, 'hours').toDate(),
-    bgcolor: '#fafafa'
+    bgcolor: '#ff9797',
+    notes: 'comprar el pastel',
+    user: {
+      _id: '123',
+      name: 'Oscar',
+    }
   }
 ]
 
 
 const CalendarScreen = () => {
+
+  const [lastView, setLastView] = useState(localStorage.getItem('lastView') || 'month')
+
+  // doble click en un evento de calendario
+  const onDoubleClick = (e) => {
+    console.log(e) //me devuelve todo el evento
+  }
+
+  // onSelect es al clickar en un evento una vez
+  const onSelect = (e) => {
+    console.log(e) //e me devuelve todo el evento
+  }
+  
+  // al cambiar de vista entre mes|semana|dia|agenda
+  const onViewChange = (e) => {
+    // console.log(e) // e devuelve week|day|month|agenda
+    setLastView(e);
+    localStorage.setItem('lastView', e);
+  }
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+    let style = {
+      backgroundColor: '#3e9edfcf',
+      borderRadius: '10px',
+      opacity: 0.8,
+      display: 'block',
+      color: 'black',
+    }
+    return {
+      style
+    }
+
+  }
+
   return (
     <div className="calendar-screen">
       <Navbar />
@@ -31,6 +76,13 @@ const CalendarScreen = () => {
         events={events}
         startAccessor="start"
         endAccessor="end"
+        messages={messages}
+        eventPropGetter={eventStyleGetter}
+        onDoubleClickEvent={onDoubleClick}
+        onSelectEvent={onSelect}
+        onView={onViewChange}
+        view={lastView}
+        components={{ event: CalendarEvent }}
       />
     </div>
   )
